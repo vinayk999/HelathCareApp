d5d93fda0476a976fc703091d0d2e4a7f5e742d5
user <user@user-PC>
2018-10-12 15:49:48 +0530

    1003

--- src/main/java/com/app/controller/EmployeeController.java
+++ src/main/java/com/app/controller/EmployeeController.java
@@ -1,25 +1,19 @@
 package com.app.controller;
 
-import org.apache.log4j.Appender;
-import org.apache.log4j.ConsoleAppender;
-import org.apache.log4j.Layout;
 import org.apache.log4j.Logger;
-import org.apache.log4j.SimpleLayout;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import com.mysql.jdbc.log.Log;
 
 @Controller
 @RequestMapping("/emp")
 public class EmployeeController {
 
-	Logger log=Logger.getLogger(EmployeeController.class);
+	private static Logger log=Logger.getLogger(EmployeeController.class);
 	
 	
 	@RequestMapping("/view")
 	public String showMsg(){
-		
+		log.info("in controller");
 		//System.out.println("in view controller");
 		log.info("in controller");
 		return "View";
